97cdc93f295b206f9b25bbe2dabc43dc4f5c57bd
Harrison <jiksdevops@gmail.com>
2023-08-19 17:22:34 +0200

    Remove used imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
-"use client";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter, Lato, Poppins, Lexend } from "next/font/google";
+import { Lexend } from "next/font/google";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import { useScroll, motion } from "framer-motion";
 
 // const inter = Inter({ subsets: ["latin"] });
-const lato = Lato({ weight: "400", subsets: ["latin-ext"] });
 const lexend = Lexend({ weight: "400", subsets: ["latin-ext"] });
 
 export const metadata: Metadata = {
@@ -16,7 +13,6 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const { scrollYProgress } = useScroll();
   return (
     <html lang='en' className='transition-all'>
       <body className={`${lexend.className}`}>
